b42c258e570889ea6bba1a7a04ac63bc4812609d
RomikSm <roma.semen.com@gmail.com>
2023-04-19 07:31:38 +0300

    Made OfflineGame fullscreen with no status bar

--- app/src/main/java/com/example/androidstudiotankgame/OfflineGame.java
+++ app/src/main/java/com/example/androidstudiotankgame/OfflineGame.java
@@ -3,6 +3,7 @@ package com.example.androidstudiotankgame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class OfflineGame extends AppCompatActivity {
     public int tank_type;
@@ -11,6 +12,11 @@ public class OfflineGame extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         tank_type = getIntent().getIntExtra("tank_type", 0);
+
+        //making fullscreen with no status bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(new Game(this, tank_type));
 
     }
